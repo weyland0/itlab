d9c524b927e44afb76e00655f77a806fc92a297f
weyland0 <philippbatkov@gmail.com>
2024-11-05 14:00:30 +0300

    Add test code

---
+++ Include/Math.h
@@ -0,0 +1,14 @@
+
+
+#ifndef TESTLIB_MATH_H
+    #define TESTLIB_MATH_H
+
+namespace tlib
+{
+    int Add(int first, int second);
+    int Sub(int first, int second);
+    int Mult(int first, int second);
+    int Div(int first, int second);
+}
+
+#endif
---
+++ Source/Math.cpp
@@ -0,0 +1,27 @@
+
+#include "Math.h"
+
+namespace tlab
+{
+
+    int Add(int first, int second)
+    {
+        return first + second;
+    }
+
+    int Sub(int first, int second)
+    {
+        return first - second; 
+    }
+
+    int Mult(int first, int second)
+    {
+        return first * second;
+    }
+
+    int Div(int first, int second)
+    {
+        return first / second;
+    }
+    
+}
